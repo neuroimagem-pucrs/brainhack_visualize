c7910403d845bc0370ba90e06cfb5a0d1bad88bb
Rômulo Bergesch Raffin <rbraffin@gmail.com>
2017-03-04 16:02:03 -0300

    Baita gambiarra

--- prototipo.py
+++ prototipo.py
@@ -20,25 +20,28 @@ out_dir = os.path.expanduser('~') + '/' + 'Downloads' + '/'
 subjects = ['SCHB009']
 
 # Criando variavel para loop while
-#direc = 0
-#sl = 0
+direc = 0
+sl = 0
 
 ####Classe botoes
 class Index(object):
 
     def bom(self, event):
+        global sl
         print 'Imagem boa.'
         dados.append(1)
         sl = sl + 1
         raise Clique
 
     def ruim(self, event):
+        global sl
         print 'Imagem ruim.'
         dados.append(2)
         sl = sl + 1
         raise Clique
 
     def voltar(self, event):
+        global sl
         print 'Retornando a imagem anterior'
         sl = sl - 1
         del dados[-1]
@@ -48,7 +51,6 @@ class Index(object):
 # Excecao para ver clique do mouse
 class Clique(Exception):
     pass
-
 
 # Iniciando laco para repetir processamento para cada sujeito
 for subj in subjects:
@@ -86,49 +88,53 @@ for subj in subjects:
     while (direc < numDir):
         # Trabalhando slice a slice na direcao
         while (sl < numSlices):
-            # Separando slice de interesse do volume
-            slice = img_data[:,:,sl,direc]
-            # Normalizando intensidades do slice para soma ser 1000000
-            slice = slice / (slice.sum()/1000000)
-            # Separando a fft do slice
-            slice_fft = fft_data[:,:,sl,0,direc]
-            # Colocando as frequencias mais baixas no centro
-            # slice_fft = fftpack.fftshift(slice_fft)
-            # Mostrando uma figura com as imagens de interesse
-            fig.clear()
-            fig.suptitle('Sujeito %s Direcao %d Slice %d' % (subj, direc, sl), fontsize=16)
-            ax1 = fig.add_subplot(121)
-            ax1.imshow(slice,cmap='gray')
-            ax1.set_title('Espaco')
-            ax1.axes.xaxis.set_visible(False)
-            ax1.axes.yaxis.set_visible(False)
-
-            ax2 = fig.add_subplot(122)
-            ax2.imshow(slice_fft,cmap='gray')
-            ax2.set_title('Frequencia')
-            ax2.axes.xaxis.set_visible(False)
-            ax2.axes.yaxis.set_visible(False)
-
-
-            bom = plt.axes([0.5, 0.05, 0.1, 0.075])
-            ruim = plt.axes([0.61, 0.05, 0.1, 0.075])
-            voltar = plt.axes([0.81, 0.05, 0.1, 0.075])
-
-            botao_bom = Button(bom, 'Bom')
-            botao_bom.on_clicked(callback.bom)
-
-            botao_ruim = Button(ruim, 'Ruim')
-            botao_ruim.on_clicked(callback.ruim)
-
-            botao_voltar = Button(voltar, 'Voltar')
-            botao_voltar.on_clicked(callback.voltar)
-
-            #Isso libera o terminal para os comandos
-            plt.show(block=False)
-            #Isto redesenha a figura a cada iteracao
-            fig.canvas.draw()
             try:
-                string = raw_input("[1] Boa/[2] Ruim ou [3] para retornar: ")
+                # Separando slice de interesse do volume
+                slice = img_data[:,:,sl,direc]
+                # Normalizando intensidades do slice para soma ser 1000000
+                slice = slice / (slice.sum()/1000000)
+                # Separando a fft do slice
+                slice_fft = fft_data[:,:,sl,0,direc]
+                # Colocando as frequencias mais baixas no centro
+                # slice_fft = fftpack.fftshift(slice_fft)
+                # Mostrando uma figura com as imagens de interesse
+                fig.clear()
+                fig.suptitle('Sujeito %s Direcao %d Slice %d' % (subj, direc, sl), fontsize=16)
+                ax1 = fig.add_subplot(121)
+                ax1.imshow(slice,cmap='gray')
+                ax1.set_title('Espaco')
+                ax1.axes.xaxis.set_visible(False)
+                ax1.axes.yaxis.set_visible(False)
+
+                ax2 = fig.add_subplot(122)
+                ax2.imshow(slice_fft,cmap='gray')
+                ax2.set_title('Frequencia')
+                ax2.axes.xaxis.set_visible(False)
+                ax2.axes.yaxis.set_visible(False)
+
+
+                bom = plt.axes([0.5, 0.05, 0.1, 0.075])
+                ruim = plt.axes([0.61, 0.05, 0.1, 0.075])
+                voltar = plt.axes([0.81, 0.05, 0.1, 0.075])
+
+                botao_bom = Button(bom, 'Bom')
+                botao_bom.on_clicked(callback.bom)
+
+                botao_ruim = Button(ruim, 'Ruim')
+                botao_ruim.on_clicked(callback.ruim)
+
+                botao_voltar = Button(voltar, 'Voltar')
+                botao_voltar.on_clicked(callback.voltar)
+
+                #Isso libera o terminal para os comandos
+                plt.show(block=False)
+                #Isto redesenha a figura a cada iteracao
+                fig.canvas.draw()
+                try:
+                    string = raw_input("[1] Boa/[2] Ruim ou [3] para retornar: ")
+                except(ValueError):
+                    print 'Nao aperte Enter!!'
+                    pass
                 if (int(string) == 1):
                     print 'Imagem boa.'
                     dados.append(1)
@@ -143,10 +149,9 @@ for subj in subjects:
                     del dados[-1]
                 else:
                     print 'Digite um comando valido.'
-            except(ValueError):
-                pass
+
             except(Clique):
-                continue
+                    pass
     # Fechando janela com as figuras
     plt.close(fig)
 # Salvando os dados em arquivo pickle
